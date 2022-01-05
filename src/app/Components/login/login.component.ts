4e4a03aadc0d6102a8eaeb816053df190aff4c65
siba98 <gyansiba.patro@gmail.com>
2022-01-06 01:20:10 +0530

    [Siba]:Added Implementation For Login API Integration

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserServiceService } from 'src/app/Services/UserService/user-service.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  show: boolean = false;
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private userService: UserServiceService) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      Email: ['', [Validators.required, Validators.email]],
+      Password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  Login() {
+    try {
+      this.userService.Login(this.loginForm.value).subscribe((response: any) => {
+        if (response.status)
+          console.log("Login Successful", response);
+      })
+    }
+    catch (error) {
+      console.log(error);
+    };
+  }
 }
--- src/app/Services/UserService/user-service.service.ts
+++ src/app/Services/UserService/user-service.service.ts
@@ -9,7 +9,6 @@ export class UserServiceService {
 
   constructor(private httpservice: HttpServiceService) { }
 
-
   baseUrl = environment.baseUrl;
 
   Register(data: any) {
@@ -20,5 +19,13 @@ export class UserServiceService {
       Password: data.Password,
     }
     return this.httpservice.post(`${this.baseUrl}/api/User/register`, param);
+  }
+
+  Login(data: any) {
+    let param = {
+      Email: data.Email,
+      Password: data.Password,
+    }
+    return this.httpservice.post(`${this.baseUrl}/api/User/login`, param);
   }
 }
